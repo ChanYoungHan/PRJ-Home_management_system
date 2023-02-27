14a52600b817ed0657af13b3530c7f434db0245a
ChanYoungHan <sanbon0812@naver.com>
2023-02-27 15:49:55 +0000

    add proxy

---
+++ proxy/app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from typing import Union
+import requests
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "control server"}
+
+@app.get("/clinets/get_connection_check")
+def read_client_info():
+    x = requests.get('http://10.28.157.3')
+    return x.json()
+
+
+
+@app.get("/get_user_credit_info/{name}")
+def read_item(name: str, q: Union[str, None] = None):
+    
+    user_info = requests.get(f'http://10.28.157.4/user_info/{name}').json()
+    credit_card_info = requests.get(f'http://10.28.157.4:81/credit_info/{user_info["credit_card_id"]}').json()
+    
+    return {
+        "user_name": name,
+        "user_id": user_info["user_id"],
+        "credit_card_id": user_info["credit_card_id"],
+        "credit_card_bank_name": credit_card_info["credit_card_bank_name"],
+    }
